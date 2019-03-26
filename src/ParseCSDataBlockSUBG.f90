0d139b85aedefa3cf47742fef814ec8ea24c890b
maxposchmann <maxposchmann@gmail.com>
2019-03-26 15:15:25 -0400

    Increase length of dummy arrays for SUBG

--- src/ParseCSDataBlockSUBG.f90
+++ src/ParseCSDataBlockSUBG.f90
@@ -4,28 +4,28 @@
     !
     !> \file    ParseCSDataBlockSUBG.f90
     !> \brief   Parse the data block section corresponding to a SUBG phase of a ChemSage data-file.
-    !> \author  M.H.A. Piro 
+    !> \author  M.H.A. Piro
     !> \date    Mar. 4, 2018
     !> \sa      ParseCSDataFile.f90
     !> \sa      ParseCSDataBlock.f90
     !> \sa      ParseCSDataBlockGibbs.f90
-    !> \todo    There are a number of lines in SUBG phases that I do not yet understand. 
-    !!           I've asked some experts and they don't know either, which tells me that 
+    !> \todo    There are a number of lines in SUBG phases that I do not yet understand.
+    !!           I've asked some experts and they don't know either, which tells me that
     !!           they're not important. Once I
     !!           gain more experience with these models, this will likely become more clear.
     !
     !
     ! DISCLAIMER
     ! ==========
-    ! 
-    ! All of the programming herein is original unless otherwise specified and is completely 
-    ! independent of ChemApp and related products, including Solgas, Solgasmix, Fact, FactSage 
-    ! and ChemSage.  
+    !
+    ! All of the programming herein is original unless otherwise specified and is completely
+    ! independent of ChemApp and related products, including Solgas, Solgasmix, Fact, FactSage
+    ! and ChemSage.
     !
     !
     ! Revisions:
     ! ==========
-    ! 
+    !
     !   Date            Programmer      Description of change
     !   ----            ----------      ---------------------
     !   03/04/2018      M.H.A. Piro     Original code
@@ -34,12 +34,12 @@
     ! Purpose:
     ! ========
     !
-    !> \details The purpose of this subroutine is to parse the "data block" section of a ChemSage data-file 
+    !> \details The purpose of this subroutine is to parse the "data block" section of a ChemSage data-file
     !! containing a "SUBG" phase, which represents the modified quasichemical model. This phase differs
     !! from many other types of thermodynamic models in that it attempts to capture Short Range Order (SRO)
     !! in liquid or solid solutions. This is achieved by focusing on pairs of species, rather than the species
     !! themselves. For more information, see the following paper:
-    !! 
+    !!
     !! A.D. Pelton, S.A. Degterov, G. Eriksson, C. Roberlin, Y. Dessureault, "The Modified Quasichemical
     !! Model I -- Binary Solutions", Metallurgical and Materials Transactions B, 31B (2000) 651-659.
     !!
@@ -49,10 +49,10 @@
     ! ====================
     !
     ! INFO                      A scalar integer that indicates a successful exit or identifies an error.
-    ! nSpeciesCS                Number of species in the system (combined solution species and pure 
+    ! nSpeciesCS                Number of species in the system (combined solution species and pure
     !                            separate phases).
     ! nGibbsEqSpecies           Number of Gibbs energy equations for a particular species.
-    ! iSpeciesAtomsCS           Integer matrix representing the number of atoms of a particular 
+    ! iSpeciesAtomsCS           Integer matrix representing the number of atoms of a particular
     !                            elements in a species.
     ! iParticlesPerMoleCS       An integer vector containing the number of particles per mole of the
     !                            constituent species formula mass.  The default value is 1.
@@ -62,22 +62,22 @@
     ! iRegularParamCS           An integer matrix representing the parameter index for the first dimension
     !                            and the mixing terms on the second dimension.  For the second dimension, the
     !                            first coefficient indicates whether the parameter is a binary or ternary term (n),
-    !                            the next n coefficients correspond to the constituent indices, and the last 
-    !                            coefficient corresponds to the exponent. 
+    !                            the next n coefficients correspond to the constituent indices, and the last
+    !                            coefficient corresponds to the exponent.
     !
     !-------------------------------------------------------------------------------------------------------------
 
 
 subroutine ParseCSDataBlockSUBG( i )
-    
+
     USE ModuleParseCS
-    
+
     implicit none
-    
+
     integer                    :: i, j
     integer,     dimension(10) :: iTempVec
-    real(8),     dimension(6)  :: dTempVec
-    character(8),dimension(3)  :: cDummyVec
+    real(8),     dimension(20)  :: dTempVec
+    character(8),dimension(20)  :: cDummyVec
 
 
     ! Initialize variables:
@@ -108,7 +108,7 @@ subroutine ParseCSDataBlockSUBG( i )
     read (1,*,IOSTAT = INFO) dTempVec(1:iTempVec(1))
 
     ! Read in the charge of each constituent on the second sublattice.
-    ! This seems unnecessary so I'm going to ignore it for now:        
+    ! This seems unnecessary so I'm going to ignore it for now:
     read (1,*,IOSTAT = INFO) dTempVec(1:iTempVec(2))
 
     ! I think that this entry represents the constituent IDs on the second sublattice (ignore for now):
@@ -138,7 +138,7 @@ subroutine ParseCSDataBlockSUBG( i )
 
         ! The end of the parameter listing is marked by "0":
         if (iRegularParamCS(nParamCS+1,1) == 0) exit LOOP_ExcessMixingSUBG
-            
+
         ! Check if the parameter is binary or ternary:
         if (iRegularParamCS(nParamCS+1,1) == 3) then
 
@@ -164,12 +164,12 @@ subroutine ParseCSDataBlockSUBG( i )
             INFO = 1600 + i
             return
         end if
-        
+
     end do LOOP_ExcessMixingSUBG
 
     ! Report an error if necessary:
-    if (INFO /= 0) INFO = 1600 + i 
-                
+    if (INFO /= 0) INFO = 1600 + i
+
     return
-    
+
 end subroutine ParseCSDataBlockSUBG
