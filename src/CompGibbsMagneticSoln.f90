dbbd5d8dc24c12c74c59f8b869e89a9954bebdde
maxposchmann <maxposchmann@gmail.com>
2020-05-11 16:27:37 -0400

    Add chemical potential contributions from excess magnetic terms

--- src/CompGibbsMagneticSoln.f90
+++ src/CompGibbsMagneticSoln.f90
@@ -63,9 +63,13 @@ subroutine CompGibbsMagneticSoln(iSolnPhaseIndex)
 
     implicit none
 
-    integer :: i, iFirst, iLast, iSolnPhaseIndex
+    integer :: i, j, iFirst, iLast, iSolnPhaseIndex, iExponent, iParam
+    integer :: k, m, n, s, c, sd, KD
+    integer :: nSublattice, iChargedPhaseID
+    integer :: iFirstParam, iSecondParam, iSubParam
     real(8) :: B, D, p, invpmone, tau, Tcritical, g, StructureFactor
     real(8) :: dTemp, dTempA, dTempB, dTempC, dTempD, dTempCoeff1, dTempCoeff2
+    real(8) :: x1, x2, dx, xprod, dxvmo, dPreFactor
     logical :: lTn, lBn
 
 
@@ -141,7 +145,146 @@ subroutine CompGibbsMagneticSoln(iSolnPhaseIndex)
             ! if (cSolnPhaseName(iSolnPhaseIndex) == 'FCC_A1') print *, cSpeciesName(i), dTemp
         end do
 
-        ! if (cSolnPhaseName(iSolnPhaseIndex) == 'FCC_A1') print *, g, Tcritical, dTempD, B
+        LOOP_Param: do iParam = nMagParamPhase(iSolnPhaseIndex-1)+1, nMagParamPhase(iSolnPhaseIndex)
+            dTempCoeff1 = dMagneticParam(iParam,1)
+            dTempCoeff2 = dMagneticParam(iParam,2)
+            if (lTn) then
+                dTempCoeff1 = -dTempCoeff1 * StructureFactor
+            end if
+            if (lBn) then
+                dTempCoeff2 = -dTempCoeff2 * StructureFactor
+            end if
+            dTemp = -(-dTempCoeff1) * dTempD
+            dTemp = g * ((dTempCoeff2 ) / (1D0 + B)) + DLOG(1D0 + B) * (dTemp)
+            print *, cSolnPhaseName(iSolnPhaseIndex), dTempCoeff1, dTempCoeff2, dTemp
+            if (cSolnPhaseType(iSolnPhaseIndex) == 'RKMPM') then
+                ! Compute temporary variables for sake of convenience:
+                if (iMagneticParam(iParam,1) == 2) then
+                    ! Binary parameter:
+                    x1    = dMolFraction(iFirst + iMagneticParam(iParam,2) - 1)
+                    x2    = dMolFraction(iFirst + iMagneticParam(iParam,3) - 1)
+                    iExponent = iMagneticParam(iParam,4)
+                    xprod = x1 * x2
+                    dx    = x1 - x2
+                    dxvmo     = dx**(iExponent-1)
+                    dxvmo     = DMIN1(dxvmo,1D30)
+                    ! Cycle if dx = 0 to prevent calculating either an INF or a NAN:
+                    if (dx == 0D0) cycle LOOP_Param
+                    do i = iFirst, iLast
+                        j = i - iFirst + 1
+                        if (j == iRegularParam(iParam,2)) then
+                            ! First species of parameter.
+                            dMagGibbsEnergy(i) = dMagGibbsEnergy(i) + dTemp * &
+                                dxvmo * ((x2 - xprod)*dx + DFLOAT(iExponent)*xprod*(1D0 - dx))
+
+                        elseif (j == iRegularParam(iParam,3)) then
+                            ! Second species of parameter.
+                            dMagGibbsEnergy(i) = dMagGibbsEnergy(i) + dTemp * &
+                                dxvmo * ((x1 - xprod)*dx - DFLOAT(iExponent)*xprod*(1D0 + dx))
+                        else
+                            ! This species does not belong to the parameter.
+                            dMagGibbsEnergy(i) = dMagGibbsEnergy(i) - dTemp * &
+                                (dx**(iExponent)) * xprod * (1D0 + DFLOAT(iExponent))
+                        end if
+
+                    end do
+                else
+                    ! The parameter index is not supported/recognized.  Report an error and exit.
+                    INFOThermo = 43
+                    exit LOOP_Param
+                end if
+            else if (cSolnPhaseType(iSolnPhaseIndex) == 'SUBLM') then
+                iChargedPhaseID = iPhaseSublattice(iSolnPhaseIndex)
+                nSublattice     = nSublatticePhase(iChargedPhaseID)
+                ! Reinitialize temporary variable:
+                dPreFactor = 1D0
+
+                ! Store the number of constituents involved in this parameter:
+                n = iMagneticParam(iParam,1)
+                iExponent = iMagneticParam(iParam,n+2)
+
+                ! Loop through constituents associated with this parameter:
+                do k = 2, n + 1
+
+                    ! Determine constituent and sublattice indices:
+                    c = MOD(iMagneticParam(iParam,k), 10000)
+                    s = iMagneticParam(iParam,k) - c
+                    s = s / 10000
+
+                    ! Compute prefactor term:
+                    dPreFactor = dPreFactor * dSiteFraction(iChargedPhaseID,s,c)
+
+                    ! Store the first and second site fractions:
+                    ! This assumes that the constituents that are mixing are the first two listed:
+                    if (k == 2) then
+                        x1 = dSiteFraction(iChargedPhaseID,s,c)
+                        iFirstParam = c
+                        iSubParam   = s
+                    elseif (k == 3) then
+                        x2 = dSiteFraction(iChargedPhaseID,s,c)
+                        iSecondParam = c
+                    end if
+                end do
+
+                ! Multiply prefactor term by excess Gibbs energy parameter:
+                dPreFactor = dPreFactor * dTemp * (x1 - x2)**iExponent
+
+                LOOP_Param_Species: do i = iFirst, iLast
+
+                    ! Reinitialize variables:
+                    KD    = 0
+                    m     = i - iFirst + 1
+                    dTemp = -DFLOAT(nSublattice + iMagneticParam(iParam,n+2))
+
+                    ! Loop through sublattices associated with this phase:
+                    LOOP_Param_Sub: do s = 1, nSublattice
+                        ! Store constituent index corresponding to component i on sublattice s:
+                        c = iConstituentSublattice(iChargedPhaseID,s,m)
+
+                        ! Assign Kronecker-Delta term if this component contains the constituent corresponding
+                        ! to the mixing parameter:
+                        if (s == iSubParam) then
+                            if (c == iFirstParam) then
+                                ! This is the first mixing constituent:
+                                KD = 1
+                            elseif (c == iSecondParam) then
+                                ! This is the second mixing constituent:
+                                KD = -1
+                            else
+                                ! The constituents don't match:
+                                KD = 0
+                            end if
+                        end if
+
+                        ! Loop through constituents involved in this mixing parameter:
+                        LOOP_Param_Const: do j = 2, n + 1
+                            k = MOD(iMagneticParam(iParam,j), 10000)  ! Constituent index corresponding to parameter.
+                            sd = iMagneticParam(iParam,j) - k
+                            sd = sd / 10000                       ! Sublattice index corresponding to parameter.
+
+                            ! Cycle if they are on different sublattices:
+                            if (sd /= s) cycle LOOP_Param_Const
+
+                            ! Cycle if they are different constituents:
+                            if (k /= c) cycle LOOP_Param_Const
+
+                            ! Include contribution from this site fraction:
+                            dTemp = dTemp + 1D0 / dSiteFraction(iChargedPhaseID,s,c)
+
+                        end do LOOP_Param_Const ! j
+                    end do LOOP_Param_Sub       ! s
+
+                    ! Apply higher order terms (only if x1 and x2 are not the same):
+                    if (x1 /= x2) then
+                        dTemp = dTemp + DFLOAT(KD * iMagneticParam(iParam,n+2)) / (x1 - x2)
+                    end if
+
+                    ! Apply partial molar excess Gibbs energy of mixing:
+                    dMagGibbsEnergy(i) = dMagGibbsEnergy(i) + dPreFactor * dTemp
+
+                end do LOOP_Param_Species       ! i
+            end if
+        end do LOOP_Param
 
     end if IF_Proceed
 
