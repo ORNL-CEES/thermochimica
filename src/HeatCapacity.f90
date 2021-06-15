dc4d3444395a817b7581ef8301c86273fec15c22
maxposchmann <maxposchmann@gmail.com>
2021-06-15 12:36:27 -0400

    Add numerical heat capacity calculation

---
+++ src/HeatCapacity.f90
@@ -0,0 +1,28 @@
+subroutine HeatCapacity(dHeatCapacity)
+
+    USE ModuleThermoIO
+    USE ModuleThermo
+
+    implicit none
+
+    integer   :: j
+    real(8), dimension(3) :: dGibbsEnergies
+    real(8), intent(out)  :: dHeatCapacity
+    real(8)               :: dTStepSize
+    real(8)               :: dtemp0
+
+    dtemp0 = dTemperature
+    dTStepSize = 1e-2
+    dGibbsEnergies = 0D0
+
+    do j=-1,1
+        dTemperature = dtemp0 + j*dTStepSize
+        call ResetThermo
+        call Thermochimica
+        dGibbsEnergies(j+2) = dGibbsEnergySys
+    end do
+
+    ! Heat capacity from 3-point stencil 2nd derivative of G
+    dHeatCapacity = -dtemp0*(dGibbsEnergies(1)+dGibbsEnergies(3)-2D0*dGibbsEnergies(2))/dTStepSize**2
+
+end subroutine HeatCapacity
