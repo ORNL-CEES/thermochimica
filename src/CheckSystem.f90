6ed097dd3a191ff62edc45e96da570ececd540c0
maxposchmann <maxposchmann@gmail.com>
2021-06-24 09:58:17 -0400

    Remove erroneous species deletion

--- src/CheckSystem.f90
+++ src/CheckSystem.f90
@@ -269,7 +269,6 @@ subroutine CheckSystem
             ! Loop through species in solution phases:
             m = 0
             LOOP_SpeciesInSolnPhase: do j = nSpeciesPhaseCS(i-1) + 1, nSpeciesPhaseCS(i)
-                if (SUM(DABS(dStoichSpeciesCS(j,1:nElemOrComp))) == 0) cycle LOOP_SpeciesInSolnPhase
                 do k = 1, nElemOrComp
                     if ((dStoichSpeciesCS(j,k) > 0).AND.(iElementSystem(k) == 0)) then
                         ! This species should not be considered
