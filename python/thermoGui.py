84ea31af0211e6b9a373b31b17876fc4d20afa5e
maxposchmann <maxposchmann@gmail.com>
2021-07-21 11:03:07 -0400

    Move element list

--- python/thermoGui.py
+++ python/thermoGui.py
@@ -3,6 +3,17 @@ import subprocess
 import math
 import os
 
+atomic_number_map = [
+    'H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al','Si','P',
+    'S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn',
+    'Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh',
+    'Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd',
+    'Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta','W','Re',
+    'Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th',
+    'Pa','U','Np','Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db',
+    'Sg','Bh','Hs','Mt','Ds','Rg','Cn','Nh','Fl','Mc','Lv','Ts', 'Og'
+]
+
 file_list_column = [
     [
         sg.Text("Database Folder"),
@@ -68,17 +79,6 @@ while True:                             # The Event Loop
                     elIt = elIt + 1
                     if elIt == nElements:
                         break
-
-            atomic_number_map = [
-            'H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al','Si','P',
-            'S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn',
-            'Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh',
-            'Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd',
-            'Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta','W','Re',
-            'Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th',
-            'Pa','U','Np','Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db',
-            'Sg','Bh','Hs','Mt','Ds','Rg','Cn','Nh','Fl','Mc','Lv','Ts', 'Og'
-            ]
 
             i = 0
             while i < nElements:
