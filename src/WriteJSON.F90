34c88894fbdd00f3d99a056ce789d1f5b4231561
maxposchmann <maxposchmann@gmail.com>
2021-08-04 10:19:36 -0400

    Write MQM data to JSON

--- src/WriteJSON.F90
+++ src/WriteJSON.F90
@@ -114,59 +114,58 @@ subroutine WriteJSONSolnPhase
             write(1,*) '      "Magnetic moment per atom": ', B, ','
         end if
 
-        ! if ((cSolnPhaseType(l)) == 'SUBG' .OR. (cSolnPhaseType(l) == 'SUBQ')) then
-        !     call CalculateCompositionSUBG(iSolnIndex=l,dMolesPairs=dMolesPairs,lPrint=.TRUE.)
-        ! end if
-
-        write(1,*) '      "species": {'
-
-        ! Print species:
-        do i = iFirst, iLast
-            write(1,*) '        "', TRIM(ADJUSTL(cSpeciesName(i))), '": {'
-            write(1,*) '          "mole fraction":', dMolFraction(i), ","
-            write(1,*) '          "chemical potential":', dChemicalPotential(i)*dIdealConstant*dTemperature, ","
-            write(1,*) '          "stoichiometry": [', (dStoichSpecies(i,c), ',', c = 1,nElements-1), &
-                                                        dStoichSpecies(i,nElements), ']'
-            if (i < iLast) then
-                write(1,*) '        },'
-            else
-                write(1,*) '        }'
-            end if
-        end do
-
-        ! Check if this phase is represented by the Compound Energy Formalism:
-        if ((csolnPhaseType(j) == 'SUBL').OR.(csolnPhaseType(j) == 'SUBLM')) then
-            write(1,*) '      },'
-            write(1,*) '      "sublattices": {'
-            ! Store the index # of the charged phase:
-            iChargedPhaseID = iPhaseSublattice(j)
-
-            ! Loop through sublattices:
-            do s = 1, nSublatticePhase(iChargedPhaseID)
-                write(intStr,*) s
-                write(1,*) '        "sublattice ', TRIM(ADJUSTL(intStr)), '": {'
-                write(1,*) '          "stoichiometric coefficient": ', dStoichSublattice(iChargedPhaseID,s), ','
-                write(1,*) '          "constituents": {'
-                ! Loop through constituents in sublattice s:
-                LOOP_SubCon: do c = 1, nConstituentSublattice(iChargedPhaseID,s)
-                    if (c < nConstituentSublattice(iChargedPhaseID,s)) then
-                        write(1,*) '            "', TRIM(ADJUSTL(cConstituentNameSUB(iChargedPhaseID,s,c))), '": ', &
-                                dSiteFraction(iChargedPhaseID,s,c), ','
-                    else
-                        write(1,*) '            "', TRIM(ADJUSTL(cConstituentNameSUB(iChargedPhaseID,s,c))), '": ', &
-                                dSiteFraction(iChargedPhaseID,s,c)
-                    end if
-                end do LOOP_SubCon
-                write(1,*) '          }'
-                if (s < nSublatticePhase(iChargedPhaseID)) then
+        if ((cSolnPhaseType(j)) == 'SUBG' .OR. (cSolnPhaseType(j) == 'SUBQ')) then
+            call WriteJSONMQM(j)
+        else
+            write(1,*) '      "species": {'
+            ! Print species:
+            do i = iFirst, iLast
+                write(1,*) '        "', TRIM(ADJUSTL(cSpeciesName(i))), '": {'
+                write(1,*) '          "mole fraction":', dMolFraction(i), ","
+                write(1,*) '          "chemical potential":', dChemicalPotential(i)*dIdealConstant*dTemperature, ","
+                write(1,*) '          "stoichiometry": [', (dStoichSpecies(i,c), ',', c = 1,nElements-1), &
+                                                            dStoichSpecies(i,nElements), ']'
+                if (i < iLast) then
                     write(1,*) '        },'
                 else
                     write(1,*) '        }'
                 end if
             end do
-            write(1,*) '      }'
-        else
-            write(1,*) '      }'
+
+            ! Check if this phase is represented by the Compound Energy Formalism:
+            if ((csolnPhaseType(j) == 'SUBL').OR.(csolnPhaseType(j) == 'SUBLM')) then
+                write(1,*) '      },'
+                write(1,*) '      "sublattices": {'
+                ! Store the index # of the charged phase:
+                iChargedPhaseID = iPhaseSublattice(j)
+
+                ! Loop through sublattices:
+                do s = 1, nSublatticePhase(iChargedPhaseID)
+                    write(intStr,*) s
+                    write(1,*) '        "sublattice ', TRIM(ADJUSTL(intStr)), '": {'
+                    write(1,*) '          "stoichiometric coefficient": ', dStoichSublattice(iChargedPhaseID,s), ','
+                    write(1,*) '          "constituents": {'
+                    ! Loop through constituents in sublattice s:
+                    LOOP_SubCon: do c = 1, nConstituentSublattice(iChargedPhaseID,s)
+                        if (c < nConstituentSublattice(iChargedPhaseID,s)) then
+                            write(1,*) '            "', TRIM(ADJUSTL(cConstituentNameSUB(iChargedPhaseID,s,c))), '": ', &
+                                    dSiteFraction(iChargedPhaseID,s,c), ','
+                        else
+                            write(1,*) '            "', TRIM(ADJUSTL(cConstituentNameSUB(iChargedPhaseID,s,c))), '": ', &
+                                    dSiteFraction(iChargedPhaseID,s,c)
+                        end if
+                    end do LOOP_SubCon
+                    write(1,*) '          }'
+                    if (s < nSublatticePhase(iChargedPhaseID)) then
+                        write(1,*) '        },'
+                    else
+                        write(1,*) '        }'
+                    end if
+                end do
+                write(1,*) '      }'
+            else
+                write(1,*) '      }'
+            end if
         end if
 
         if (j < nSolnPhasesSys) then
@@ -223,3 +222,194 @@ subroutine WriteJSONPureConPhase
     write(1,*) '  },'
 
 end subroutine WriteJSONPureConPhase
+
+subroutine WriteJSONMQM(iSolnIndex)
+
+    USE ModuleThermo
+    USE ModuleThermoIO
+    USE ModuleGEMSolver
+
+    implicit none
+
+    integer :: i, j, k, l, m, c
+    integer :: iSolnIndex, iSPI, nPhaseElements
+    integer :: iFirst, iLast, nSub1, nSub2, iMax
+    real(8) :: dSum, dMax, dSumElementQuads, dSumElementPairs,dMolesPairs
+    real(8) :: dZa, dZb, dZx, dZy
+    real(8), allocatable, dimension(:) :: dXi, dYi, dNi
+    real(8), allocatable, dimension(:,:) :: dXij, dNij
+    ! X_ij/kl corresponds to dMolFraction
+
+    ! Only proceed if the correct phase type is selected:
+    if (.NOT. (cSolnPhaseType(iSolnIndex) == 'SUBG' .OR. cSolnPhaseType(iSolnIndex) == 'SUBQ')) return
+
+    ! Define temporary variables for sake of convenience:
+    iFirst = nSpeciesPhase(iSolnIndex-1) + 1
+    iLast  = nSpeciesPhase(iSolnIndex)
+    iSPI = iPhaseSublattice(iSolnIndex)
+    nSub1 = nSublatticeElements(iSPI,1)
+    nSub2 = nSublatticeElements(iSPI,2)
+
+    ! Allocate allocatable arrays:
+    if (allocated(dXi)) deallocate(dXi)
+    if (allocated(dYi)) deallocate(dYi)
+    if (allocated(dNi)) deallocate(dNi)
+    if (allocated(dXij)) deallocate(dXij)
+    if (allocated(dNij)) deallocate(dNij)
+    j = iLast - iFirst + 1
+    nPhaseElements = nSub1 + nSub2
+    allocate(dXi(nPhaseElements),dYi(nPhaseElements),dNi(nPhaseElements))
+    allocate(dXij(nSub1,nSub2))
+    allocate(dNij(nSub1,nSub2))
+
+    ! Initialize variables:
+    dXi                               = 0D0
+    dYi                               = 0D0
+    dNi                               = 0D0
+    dXij                              = 0D0
+    dNij                              = 0D0
+
+    ! Compute X_i and Y_i
+    ! Do cations first:
+    dSum = 0D0
+    do i = 1, nSub1
+        do k = 1, nPairsSRO(iSPI,2)
+            l = iFirst + k - 1
+            dZa = dCoordinationNumber(iSPI,k,1)
+            dZb = dCoordinationNumber(iSPI,k,2)
+            if (i == iPairID(iSPI,k,1))  then
+                dNi(i) = dNi(i) + (dMolFraction(l) / dZa)
+                dYi(i) = dYi(i) + (dMolFraction(l) / 2)
+            end if
+            if (i == iPairID(iSPI,k,2))  then
+                dNi(i) = dNi(i) + (dMolFraction(l) / dZb)
+                dYi(i) = dYi(i) + (dMolFraction(l) / 2)
+            end if
+        end do
+        dSum = dSum + dNi(i)
+    end do
+    write(1,*) '      "cation fractions": {'
+    do i = 1, nSub1
+        dXi(i) = dNi(i) / dSum
+        if (i < nSub1) then
+            write(1,*) '        "', TRIM(ADJUSTL(cConstituentNameSUB(iSPI,1,i))), '": ', dXi(i), ','
+        else
+            write(1,*) '        "', TRIM(ADJUSTL(cConstituentNameSUB(iSPI,1,i))), '": ', dXi(i)
+        end if
+    end do
+    write(1,*) '      },'
+    ! Do anions now:
+    dSum = 0D0
+    do i = 1, nSub2
+        j = i + nSub1
+        do k = 1, nPairsSRO(iSPI,2)
+            l = iFirst + k - 1
+            dZx = dCoordinationNumber(iSPI,k,3)
+            dZy = dCoordinationNumber(iSPI,k,4)
+            if (j == iPairID(iSPI,k,3))  then
+                dNi(j) = dNi(j) + (dMolFraction(l) / dZx)
+                dYi(j) = dYi(j) + (dMolFraction(l) / 2)
+            end if
+            if (j == iPairID(iSPI,k,4))  then
+                dNi(j) = dNi(j) + (dMolFraction(l) / dZy)
+                dYi(j) = dYi(j) + (dMolFraction(l) / 2)
+            end if
+        end do
+        dSum = dSum + dNi(j)
+    end do
+    write(1,*) '      "anion fractions": {'
+    do i = 1, nSub2
+        j = i + nSub1
+        dXi(j) = dNi(j) / dSum
+        if (i < nSub2) then
+            write(1,*) '        "', TRIM(ADJUSTL(cConstituentNameSUB(iSPI,2,i))), '": ', dXi(j), ','
+        else
+            write(1,*) '        "', TRIM(ADJUSTL(cConstituentNameSUB(iSPI,2,i))), '": ', dXi(j)
+        end if
+    end do
+    write(1,*) '      },'
+
+    dSum = 0D0
+    do m = 1, nPairsSRO(iSPI,1)
+        i = iConstituentSublattice(iSPI,1,m)
+        j = iConstituentSublattice(iSPI,2,m)
+        do k = 1, nPairsSRO(iSPI,2)
+            l = iFirst + k - 1
+            dZa = dCoordinationNumber(iSPI,k,1)
+            dZb = dCoordinationNumber(iSPI,k,2)
+            if ((i == iPairID(iSPI,k,1)) .AND. ((j + nSub1) == iPairID(iSPI,k,3)))  then
+                dNij(i,j) = dNij(i,j) + (dMolFraction(l) / dZa) / dConstituentCoefficients(iSPI,m,1)
+            end if
+            if ((i == iPairID(iSPI,k,1)) .AND. ((j + nSub1) == iPairID(iSPI,k,4)))  then
+                dNij(i,j) = dNij(i,j) + (dMolFraction(l) / dZa) / dConstituentCoefficients(iSPI,m,1)
+            end if
+            if ((i == iPairID(iSPI,k,2)) .AND. ((j + nSub1) == iPairID(iSPI,k,3)))  then
+                dNij(i,j) = dNij(i,j) + (dMolFraction(l) / dZb) / dConstituentCoefficients(iSPI,m,1)
+            end if
+            if ((i == iPairID(iSPI,k,2)) .AND. ((j + nSub1) == iPairID(iSPI,k,4)))  then
+                dNij(i,j) = dNij(i,j) + (dMolFraction(l) / dZb) / dConstituentCoefficients(iSPI,m,1)
+            end if
+        end do
+        dSum = dSum + dNij(i,j)
+    end do
+
+    ! Use most abundant element in phase to normalize
+    dMax = 0D0
+    do i = 1, nElements
+        dSumElementQuads = 0D0
+        do k = 1, nPairsSRO(iSPI,2)
+            l = iFirst + k - 1
+            dSumElementQuads = dSumElementQuads + dStoichSpecies(l,i)*dMolesSpecies(l)
+        end do
+        if (dSumElementQuads > dMax) then
+            dMax = dSumElementQuads
+            iMax = i
+        end if
+    end do
+    dSumElementQuads = dMax
+
+    dSumElementPairs = 0D0
+    do m = 1, nPairsSRO(iSPI,1)
+        i = iConstituentSublattice(iSPI,1,m)
+        j = iConstituentSublattice(iSPI,2,m)
+        dSumElementPairs = dSumElementPairs + dStoichPairs(iSPI,m,iMax)*dNij(i,j)
+    end do
+
+    dMolesPairs = dSum*dSumElementQuads/dSumElementPairs
+    write(1,*) '      "moles of endmembers": ', dMolesPairs, ','
+    write(1,*) '      "endmember fractions": {'
+    do m = 1, nPairsSRO(iSPI,1)
+        i = iConstituentSublattice(iSPI,1,m)
+        j = iConstituentSublattice(iSPI,2,m)
+        dXij(i,j) = dNij(i,j) / dSum
+        if (m < nPairsSRO(iSPI,1)) then
+            write(1,*) '        "', TRIM(ADJUSTL(cPairName(iSPI,m))), '": ', dXij(i,j), ','
+        else
+            write(1,*) '        "', TRIM(ADJUSTL(cPairName(iSPI,m))), '": ', dXij(i,j)
+        end if
+    end do
+    write(1,*) '      },'
+
+    write(1,*) '      "quadruplets": {'
+    ! Print species:
+    do i = iFirst, iLast
+        k = i + 1 - iFirst
+        write(1,*) '        "', TRIM(ADJUSTL(cSpeciesName(i))), '": {'
+        write(1,*) '          "mole fraction":', dMolFraction(i), ","
+        write(1,*) '          "chemical potential":', dChemicalPotential(i)*dIdealConstant*dTemperature, ','
+        write(1,*) '          "coordination numbers": [', (dCoordinationNumber(iSPI,k,c), ',', c = 1,3), &
+                                                    dCoordinationNumber(iSPI,k,4), '],'
+        write(1,*) '          "stoichiometry": [', (dStoichSpecies(i,c), ',', c = 1,nElements-1), &
+                                                    dStoichSpecies(i,nElements), ']'
+        if (i < iLast) then
+            write(1,*) '        },'
+        else
+            write(1,*) '        }'
+        end if
+    end do
+    write(1,*) '      }'
+
+    ! Deallocate allocatable arrays:
+    deallocate(dXi,dYi,dNi,dXij,dNij)
+
+end subroutine WriteJSONMQM
