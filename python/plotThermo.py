9d473d4c3a0db25d44c58b300e9b217a78714cbf
maxposchmann <maxposchmann@gmail.com>
2021-08-05 14:20:10 -0400

    Create selection window for species

--- python/plotThermo.py
+++ python/plotThermo.py
@@ -27,47 +27,93 @@ while True:
         break
     elif event == '-yaxis-':
         ykey = [[]]
+        yen = []
+        plotWindow.Element('Plot').Update(disabled = True)
         if values['-yaxis-'] == 'temperature' or values['-yaxis-'] == 'pressure':
             ykey[0].append(values['-yaxis-'])
+            yen.append(True)
             plotWindow.Element('Plot').Update(disabled = False)
         elif values['-yaxis-'] == 'moles' or values['-yaxis-'] == 'chemical potential':
             ykey = []
             solutionPhases = list(data['1']['solution phases'].keys())
             pureCondensedPhases = list(data['1']['pure condensed phases'].keys())
+            phaseColumns = []
+            yi = 0
             for j in solutionPhases:
+                phaseColumns.append([[sg.Text(j)]])
                 if data['1']['solution phases'][j]['phase model'] == 'SUBG' or data['1']['solution phases'][j]['phase model'] == 'SUBQ':
                     speciesLabel = 'quadruplets'
                 else:
                     speciesLabel = 'species'
                 for k in list(data['1']['solution phases'][j][speciesLabel].keys()):
                     ykey.append(['solution phases',j,speciesLabel,k,values['-yaxis-']])
+                    yen.append(False)
+                    phaseColumns[-1].append([sg.Checkbox(ykey[yi][-2],key=str(yi))])
+                    yi = yi + 1
+            phaseColumns.append([[sg.Text('Pure Condensed Phases')]])
             for j in pureCondensedPhases:
                 ykey.append(['pure condensed phases',j,values['-yaxis-']])
-            plotWindow.Element('Plot').Update(disabled = False)
+                yen.append(False)
+                phaseColumns[-1].append([sg.Checkbox(ykey[yi][-2],key=str(yi))])
+                yi = yi + 1
+            phaseSelectLayout = [[]]
+            for j in phaseColumns:
+                phaseSelectLayout[0].append(sg.Column(j,vertical_alignment='t'))
+            phaseSelectLayout.append([sg.Button('Accept'), sg.Button('Cancel')])
+            selectWindow = sg.Window('Thermochimica species selection', phaseSelectLayout, finalize=True)
+            while True:
+                event, values = selectWindow.read()
+                if event == sg.WIN_CLOSED or event == 'Cancel':
+                    break
+                elif event == 'Accept':
+                    for yi in range(len(ykey)):
+                        yen[yi] = values[str(yi)]
+                    plotWindow.Element('Plot').Update(disabled = False)
+                    break
+            selectWindow.close()
         elif values['-yaxis-'] == 'mole fraction':
             ykey = []
             solutionPhases = list(data['1']['solution phases'].keys())
-            pureCondensedPhases = list(data['1']['pure condensed phases'].keys())
+            phaseColumns = []
+            yi = 0
             for j in solutionPhases:
+                phaseColumns.append([[sg.Text(j)]])
                 if data['1']['solution phases'][j]['phase model'] == 'SUBG' or data['1']['solution phases'][j]['phase model'] == 'SUBQ':
                     speciesLabel = 'quadruplets'
                 else:
                     speciesLabel = 'species'
                 for k in list(data['1']['solution phases'][j][speciesLabel].keys()):
                     ykey.append(['solution phases',j,speciesLabel,k,values['-yaxis-']])
+                    yen.append(False)
+                    phaseColumns[-1].append([sg.Checkbox(ykey[yi][-2],key=str(yi))])
+                    yi = yi + 1
+                phaseSelectLayout = [[]]
+                for j in phaseColumns:
+                    phaseSelectLayout[0].append(sg.Column(j,vertical_alignment='t'))
+                phaseSelectLayout.append([sg.Button('Accept'), sg.Button('Cancel')])
+                selectWindow = sg.Window('Thermochimica species selection', phaseSelectLayout, finalize=True)
+                while True:
+                    event, values = selectWindow.read()
+                    if event == sg.WIN_CLOSED or event == 'Cancel':
+                        break
+                    elif event == 'Accept':
+                        for yi in range(len(ykey)):
+                            yen[yi] = values[str(yi)]
+                        plotWindow.Element('Plot').Update(disabled = False)
+                        break
+                selectWindow.close()
             plotWindow.Element('Plot').Update(disabled = False)
         elif values['-yaxis-'] == 'element potential':
             ykey = []
             elements = list(data['1']['elements'].keys())
             for j in elements:
                 ykey.append(['elements',j,values['-yaxis-']])
+                yen.append(True)
             plotWindow.Element('Plot').Update(disabled = False)
     elif event == 'Plot':
         x = []
         y = []
         xkey = values['-xaxis-']
-        print(xkey)
-        print(ykey)
         for yi in range(len(ykey)):
             y.append([])
         for j in data.keys():
@@ -82,11 +128,11 @@ while True:
                     y[yi].append(data[j][ykey[yi][0]][ykey[yi][1]][ykey[yi][2]])
                 elif len(ykey[yi]) == 5:
                     y[yi].append(data[j][ykey[yi][0]][ykey[yi][1]][ykey[yi][2]][ykey[yi][3]][ykey[yi][4]])
-        print(x)
-        print(y)
         # Start figure
         fig = plt.figure()
         ax  = fig.add_axes([0.12, 0.1, 0.85, 0.85])
         for yi in range(len(ykey)):
-            ax.plot(x,y[yi],'.-')
+            if yen[yi]:
+                ax.plot(x,y[yi],'.-')
         plt.show()
+plotWindow.close()
