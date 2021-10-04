0fc8560f6e579104f672ec8904285c1f101c69db
maxposchmann <maxposchmann@gmail.com>
2021-10-04 15:31:11 -0400

    Do the advertised check on removing last phase with element

--- src/gem/CheckPhaseChange.f90
+++ src/gem/CheckPhaseChange.f90
@@ -48,7 +48,7 @@ subroutine CheckPhaseChange(lPhasePass,INFO)
 
     implicit none
 
-    integer  ::  i, j, INFO, nMiscPhases
+    integer  ::  i, j, k, INFO, nMiscPhases
     real(8)  ::  dTemp
     logical  ::  lPhasePass
 
@@ -57,6 +57,24 @@ subroutine CheckPhaseChange(lPhasePass,INFO)
     INFO        = 0
     nMiscPhases = 0
     lPhasePass  = .TRUE.
+
+    ! Get current effective stoichiometry matrix
+    do k = 1, nSolnPhases
+        j = -iAssemblage(nElements + 1 - k)
+        call CompStoichSolnPhase(j)
+    end do
+    ! if we are removing any elements completely, don't allow change
+    ElementCheck: do i = 1, nElements
+        do k = 1, nConPhases
+            j = iAssemblage(k)
+            if (dStoichSpecies(j,i) > 0) cycle ElementCheck
+        end do
+        do k = 1, nSolnPhases
+            j = -iAssemblage(nElements + 1 - k)
+            if (dEffStoichSolnPhase(j,i) > 0) cycle ElementCheck
+        end do
+        lPhasePass = .FALSE.
+    end do ElementCheck
 
     ! Count the number of miscible phases:
     do i = 1, nSolnPhases
