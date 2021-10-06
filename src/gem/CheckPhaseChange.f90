5035ef5532635f1fc780d9d6ec325c119022ebf4
maxposchmann <maxposchmann@gmail.com>
2021-10-06 15:32:01 -0400

    Further improve phase counts

--- src/gem/CheckPhaseChange.f90
+++ src/gem/CheckPhaseChange.f90
@@ -59,12 +59,25 @@ subroutine CheckPhaseChange(lPhasePass,INFO)
     lPhasePass  = .TRUE.
 
     ! Count phases:
-    nSolnPhases = 0
+    j = nConPhases
     nConPhases  = 0
-    do i = 1, nElements
-        if (iAssemblage(i) < 0) nSolnPhases = nSolnPhases + 1
-        if (iAssemblage(i) > 0) nConPhases  = nConPhases  + 1
-    end do
+    CountCon: do i = 1, j
+        if (iAssemblage(i) > 0) then
+            nConPhases  = nConPhases  + 1
+        else
+            exit CountCon
+        end if
+    end do CountCon
+
+    j = nSolnPhases
+    nSolnPhases = 0
+    CountSoln: do i = nElements, nElements + 1 - j, -1
+        if (iAssemblage(i) < 0) then
+            nSolnPhases = nSolnPhases + 1
+        else
+            exit CountSoln
+        end if
+    end do CountSoln
 
     ! Count the number of miscible phases:
     do i = 1, nSolnPhases
