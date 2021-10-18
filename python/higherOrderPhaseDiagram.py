7b27ac76cefbd69815e255403828e9cc543e7277
maxposchmann <maxposchmann@gmail.com>
2021-10-18 18:13:15 -0400

    Add work on higher-order phase diagrams

---
+++ python/higherOrderPhaseDiagram.py
@@ -0,0 +1,210 @@
+import PySimpleGUI as sg
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import os
+import subprocess
+import scipy.optimize
+
+atomic_number_map = [
+    'H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al','Si','P',
+    'S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn',
+    'Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh',
+    'Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd',
+    'Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta','W','Re',
+    'Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th',
+    'Pa','U','Np','Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db',
+    'Sg','Bh','Hs','Mt','Ds','Rg','Cn','Nh','Fl','Mc','Lv','Ts', 'Og'
+]
+
+def line_intersection(line1, lines):
+    l1 = np.array(line1)
+    ls = np.array(lines)
+    def diff(mu):
+        sum = l1[0] + mu[0]*(l1[1] - l1[0])
+        for i in range(nElements - 2):
+            sum -= ls[i][0] + mu[i+1]*(ls[i][1] - ls[i][0])
+        return sum
+    return scipy.optimize.least_squares(diff, [0.5 for i in range(nElements-1)]).x
+
+timeout = 50
+inputSize = 20
+
+windowList = []
+
+class DataWindow:
+    def __init__(self):
+        windowList.append(self)
+        file_list_column = [
+            [
+                sg.Text("Database Folder"),
+                sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+                sg.FolderBrowse(),
+            ],
+            [
+                sg.Listbox(
+                    values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+                )
+            ],
+        ]
+        self.folder = os.getcwd()+'/data'
+        try:
+            file_list = os.listdir(self.folder)
+        except:
+            file_list = []
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(self.folder, f))
+            and f.lower().endswith((".dat", ".DAT"))
+        ]
+        self.sgw = sg.Window('Thermochimica database selection', file_list_column, location = [0,0], finalize=True)
+        self.sgw["-FILE LIST-"].update(fnames)
+        self.children = []
+    def close(self):
+        for child in self.children:
+            child.close()
+        self.sgw.close()
+        if self in windowList:
+            windowList.remove(self)
+    def read(self):
+        event, values = self.sgw.read(timeout=timeout)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            self.close()
+        elif event == "-FOLDER-":
+            self.folder = values["-FOLDER-"]
+            try:
+                file_list = os.listdir(self.folder)
+            except:
+                file_list = []
+
+            fnames = [
+                f
+                for f in file_list
+                if os.path.isfile(os.path.join(self.folder, f))
+                and f.lower().endswith((".dat", ".DAT"))
+            ]
+            self.sgw["-FILE LIST-"].update(fnames)
+        elif event == "-FILE LIST-":  # A file was chosen from the listbox
+            try:
+                datafile = os.path.join(
+                    self.folder, values["-FILE LIST-"][0]
+                )
+                with open(datafile) as f:
+                    f.readline() # read comment line
+                    line = f.readline() # read first data line (# elements, # phases, n*# species)
+                    nElements = int(line[1:5])
+                    nSoln = int(line[6:10])
+                    elements = []
+                    for i in range(math.ceil((nSoln+3)/15)-1):
+                        f.readline() # read the rest of the # species but don't need them)
+                    for i in range(math.ceil(nElements/3)):
+                        els = f.readline() # read a line of elements (3 per line)
+                        elLen = 25 # formatted 25 wide
+                        for j in range(3):
+                            elements.append(els[1+j*elLen:(1+j)*elLen].strip())
+                i = 0
+                while i < nElements:
+                    try:
+                        index = atomic_number_map.index(elements[i])+1 # get element indices in PT (i.e. # of protons)
+                        i = i + 1
+                    except ValueError:
+                        print(elements[i]+' not in list') # if the name is bogus (or e(phase)), discard
+                        elements.remove(elements[i])
+                        nElements = nElements - 1
+                tempLayout = [sg.Column([[sg.Text('Temperature')],[sg.Input(key='-temperature-',size=(inputSize,1))],
+                              [sg.Text('Temperature unit')],[sg.Combo(['K', 'C', 'F'],default_value='K',key='-tunit-')]],
+                              vertical_alignment='t'),
+                              sg.Column([[sg.Text('End Temperature',key='-endtemperaturelabel-')],
+                              [sg.Input(key='-endtemperature-',size=(inputSize,1))]],
+                              vertical_alignment='t'),
+                              sg.Column([[sg.Text('# of steps',key='-tsteplabel-')],[sg.Input(key='-ntstep-',size=(8,1))]],
+                              vertical_alignment='t')]
+                presLayout = [sg.Column([[sg.Text('Pressure')],[sg.Input(key='-pressure-',size=(inputSize,1))],
+                              [sg.Text('Pressure unit')],[sg.Combo(['atm', 'Pa', 'bar'],default_value='atm',key='-punit-')]],
+                              vertical_alignment='t')]
+                elem1Layout = [[sg.Text('Composition 1')]]
+                elem2Layout = [[sg.Text('Composition 2')]]
+                for i in range(nElements):
+                    elem1Layout.append([sg.Text(elements[i])])
+                    elem1Layout.append([sg.Input(key='-'+elements[i]+'1-',size=(inputSize,1))])
+                for i in range(nElements):
+                    elem2Layout.append([sg.Text(elements[i])])
+                    elem2Layout.append([sg.Input(key='-'+elements[i]+'2-',size=(inputSize,1))])
+                if (nElements < 8):
+                    calcLayout = [tempLayout,
+                                  presLayout,
+                                  [sg.Column(elem1Layout),sg.Column(elem2Layout)],
+                                  [sg.Text('Mass unit')],
+                                  [sg.Combo(['moles'],default_value='moles',key='-munit-')],
+                                  [sg.Checkbox('Save JSON',key='-json-')],
+                                  [sg.Button('Run'), sg.Exit()]]
+                else:
+                    calcLayout = [tempLayout,
+                                  presLayout,
+                                  [sg.Column(elem1Layout,vertical_alignment='t', scrollable = True, vertical_scroll_only = True, expand_y = True)],
+                                  [sg.Text('Mass unit')],
+                                  [sg.Combo(['moles'],default_value='moles',key='-munit-')],
+                                  [sg.Checkbox('Save JSON',key='-json-')],
+                                  [sg.Button('Run'), sg.Exit()]]
+                calcWindow = CalculationWindow(calcLayout,datafile,nElements,elements)
+                self.children.append(calcWindow)
+            except:
+                pass
+
+class CalculationWindow:
+    def __init__(self, windowLayout, datafile, nElements, elements):
+        windowList.append(self)
+        self.datafile = datafile
+        self.nElements = nElements
+        self.elements = elements
+        self.sgw = sg.Window(f'Thermochimica calculation: {os.path.basename(datafile)}', windowLayout, location = [400,0], finalize=True)
+        self.children = []
+    def close(self):
+        for child in self.children:
+            child.close()
+        self.sgw.close()
+        if self in windowList:
+            windowList.remove(self)
+    def read(self):
+        event, values = self.sgw.read(timeout=timeout)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            self.close()
+        elif event =='Run':
+                temperature = values['-temperature-']
+                pressure = values['-pressure-']
+                filename = 'inputs/pythonInput.ti'
+                masses1 = [0.0]*self.nElements
+                masses2 = [0.0]*self.nElements
+                elementsUsed = []
+                for i in range(self.nElements):
+                    if values['-'+self.elements[i]+'1-'] != '':
+                        masses1[i] = values['-'+self.elements[i]+'1-']
+                    if values['-'+self.elements[i]+'2-'] != '':
+                        masses2[i] = values['-'+self.elements[i]+'2-']
+                    if masses1[i] or masses2[i]:
+                        elementsUsed.append(self.elements[i])
+                tunit = values['-tunit-']
+                punit = values['-punit-']
+                munit = values['-munit-']
+                tend = values['-endtemperature-']
+                ntstep = values['-ntstep-']
+                with open(filename, 'w') as inputFile:
+                    inputFile.write('! Python-generated input file for Thermochimica\n')
+                    inputFile.write(f'data file         = {datafile}\n')
+                    inputFile.write(f'temperature unit         = {tunit}\n')
+                    inputFile.write(f'pressure unit          = {punit}\n')
+                    inputFile.write(f'mass unit          = \'{munit}\'\n')
+                    inputFile.write(f'nEl         = {nElements} \n')
+                    inputFile.write(f'iEl         = {atomic_number_map.index(el1)+1} {atomic_number_map.index(el2)+1} {atomic_number_map.index(el3)+1}\n')
+                    inputFile.write(f'nCalc       = {len(xs)*len(temperature)}\n')
+                    for t in temperature:
+                        for x in xs:
+                            inputFile.write(f'{t} {pressure} {(1-x3)*(1-x)} {(1-x3)*x} {x3}\n')
+                subprocess.run(['./bin/RunCalculationList',filename])
+
+dataWindow = DataWindow()
+while len(windowList) > 0:
+    for window in windowList:
+        window.read()
---
+++ python/ternaryPhaseDiagramTest.py
@@ -0,0 +1,161 @@
+import PySimpleGUI as sg
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import os
+import subprocess
+import scipy.optimize
+
+fname = 'pdmoru700-1000example.json'
+elements = ['Mo', 'Pd', 'Ru']
+nElements = len(elements)
+
+mint = 1e5
+maxt = 0
+
+# For boundaries of phase regions where both sides have (# phases) < (# elements), only plot points within phaseFractionTol of the boundary
+phaseFractionTol = 1e-2
+
+plane = np.array([[0,0.5,0.5],[0.5,0,0.5]])
+# plane = np.array([[0.05,0.00,0.95],[0.05,0.95,0.00]])
+
+def line_intersection(line1, lines):
+    l1 = np.array(line1)
+    ls = np.array(lines)
+    def diff(mu):
+        sum = l1[0] + mu[0]*(l1[1] - l1[0])
+        for i in range(nElements - 2):
+            sum -= ls[i][0] + mu[i+1]*(ls[i][1] - ls[i][0])
+        return sum
+    # try:
+    result = scipy.optimize.least_squares(diff, [0.5 for i in range(nElements-1)]).x
+    # except:
+    #     result = [-1000 for i in range(nElements-1)]
+    #     print()
+    return result
+
+x = []
+p = []
+ts = []
+
+f = open(fname,)
+data = json.load(f)
+f.close()
+if list(data.keys())[0] != '1':
+    print('Output does not contain data series')
+    exit()
+for i in list(data.keys()):
+    mint = min(mint,data[i]['temperature'])
+    maxt = max(maxt,data[i]['temperature'])
+    if (data[i]['# solution phases'] + data[i]['# pure condensed phases']) == nElements:
+        allPhases = []
+        phaseComps = []
+        for phaseType in ['solution phases','pure condensed phases']:
+            for phaseName in list(data[i][phaseType].keys()):
+                if (data[i][phaseType][phaseName]['moles'] > 0):
+                    allPhases.append(phaseName)
+                    tempComp = []
+                    for element in elements:
+                        tempComp.append(data[i][phaseType][phaseName]['elements'][element]['mole fraction of phase by element'])
+                    phaseComps.append(tempComp)
+        boundPhases = []
+        boundComps = []
+        # Loop over possible phase zone intersections with plane of interest
+        for j in range(nElements):
+            # Make list of phases on a (nElements-1) dimensional face through omission
+            omitComps = phaseComps.copy()
+            omitComps.remove(phaseComps[j])
+            omitPhase = allPhases.copy()
+            omitPhase.remove(allPhases[j])
+            lineComps = []
+            for k in range(nElements - 2):
+                lineComps.append([omitComps[0],omitComps[k+1]])
+            # Calculate intersection of this face with our plane of interest
+            intersect = line_intersection(plane,lineComps)
+            # Check that the intersection is within the valid bounds
+            intSum = sum(intersect[1:])
+            intTest = intSum <= 1
+            for test in range(nElements - 1):
+                intTest = intTest and (0 <= intersect[test]) and (intersect[test] <= 1)
+            if intTest:
+                if intSum == 1:
+                    # If we are on the far boundary, the first phase is not included
+                    omitPhase.remove(omitPhase[0])
+                for k in range(nElements - 2):
+                    # Check all the other boundaries
+                    if intersect[k+1] == 0:
+                        # If none of this is used, it is not included
+                        omitPhase.remove(omitPhase[k+1])
+                ts.append(data[i]['temperature'])
+                p.append(omitPhase)
+                x.append(intersect[0])
+    elif (data[i]['# solution phases'] + data[i]['# pure condensed phases']) > 0:
+        boundPhases = []
+        skipPoint = False
+        phaseMoleSum = 0
+        for phaseType in ['solution phases','pure condensed phases']:
+            for phaseName in list(data[i][phaseType].keys()):
+                phaseMoleSum += data[i][phaseType][phaseName]['moles']
+        for phaseType in ['solution phases','pure condensed phases']:
+            for phaseName in list(data[i][phaseType].keys()):
+                if data[i][phaseType][phaseName]['moles'] > 0:
+                    boundPhases.append(phaseName)
+                    if phaseFractionTol < data[i][phaseType][phaseName]['moles']/phaseMoleSum < (1-phaseFractionTol):
+                        skipPoint = True
+                        break
+            if skipPoint:
+                break
+        if skipPoint:
+            boundPhases = []
+            continue
+        tempComp = np.zeros(nElements)
+        for e in range(len(elements)):
+            if elements[e] in data[i]['elements'].keys():
+                tempComp[e] = data[i]['elements'][elements[e]]['moles']
+        boundComps = np.linalg.norm(tempComp-plane[0])/np.linalg.norm(plane[1]-plane[0])
+        ts.append(data[i]['temperature'])
+        p.append(boundPhases)
+        x.append(boundComps)
+
+
+
+boundaries = []
+b = []
+for i in range(len(x)):
+    repeat = False
+    for j in range(len(boundaries)):
+        thisMatch = True
+        if not (len(p[i]) == len(boundaries[j])):
+            continue
+        for k in range(len(p[i])):
+            if not (p[i][k] in boundaries[j]):
+                thisMatch = False
+                break
+        if thisMatch:
+            b.append(j)
+            repeat = True
+    if not(repeat):
+        b.append(len(boundaries))
+        boundaries.append(p[i])
+
+# Start figure
+fig = plt.figure()
+# plt.ion()
+ax = fig.add_axes([0.2, 0.1, 0.75, 0.85])
+
+for j in range(len(boundaries)):
+    inds = [i for i, k in enumerate(b) if k == j]
+    if len(inds) < 2:
+        continue
+    ax.plot(np.array(x)[inds],np.array(ts)[inds],'.')
+
+ax.set_xlim(0,1)
+# ax.set_ylim(mint,maxt)
+# ax.set_title(str(el1) + ' + ' + str(el2) + ' binary phase diagram')
+# ax.set_xlabel('Mole fraction ' + str(el2))
+ax.set_ylabel('Temperature [K]')
+# for lab in labels:
+#     plt.text(float(lab[0][0]),float(lab[0][1]),lab[1], ha="center")
+plt.show()
+plt.pause(0.001)
