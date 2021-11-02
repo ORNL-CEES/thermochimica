90493d8c83ce852b593de809670ce80fd9f66413
maxposchmann <maxposchmann@gmail.com>
2021-11-02 16:00:20 -0400

    Apparently I wasn't using that function

--- python/isothermalTernaryGui.py
+++ python/isothermalTernaryGui.py
@@ -386,26 +386,6 @@ class CalculationWindow:
         subprocess.run(['./bin/Phase3DiagramDataGen',self.inputFileName])
         print('Thermochimica calculation finished.')
         self.processPhaseDiagramData()
-    def clockwiseangle_and_distance(self,point):
-        # Vector between point and the origin: v = p - o
-        vector = [point[0]-origin[0], point[1]-origin[1]]
-        # Length of vector: ||v||
-        lenvector = math.hypot(vector[0], vector[1])
-        # If length is zero there is no angle
-        if lenvector == 0:
-            return -math.pi, 0
-        # Normalize vector: v/||v||
-        normalized = [vector[0]/lenvector, vector[1]/lenvector]
-        dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]     # self.x1*self.x2 + y1*y2
-        diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]     # self.x1*y2 - y1*self.x2
-        angle = math.atan2(diffprod, dotprod)
-        # Negative angles represent counter-clockwise angles so we need to subtract them
-        # from 2*pi (360 degrees)
-        if angle < 0:
-            return 2*math.pi+angle, lenvector
-        # Return first the angle because that's the primary sorting criterion
-        # but if two vectors have the same angle then the shorter distance should come first.
-        return angle, lenvector
     def makePlot(self):
         boundaries = []
         phases = []
