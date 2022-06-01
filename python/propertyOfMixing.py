2423146bd3afb8aba74b455d21e9f108740f220b
maxposchmann <maxposchmann@gmail.com>
2022-06-01 14:53:52 -0400

    Separate endpoints from mixtures, return value

--- python/propertyOfMixing.py
+++ python/propertyOfMixing.py
@@ -22,6 +22,7 @@ def propertyOfMixing(property, phase, temperature, endpoints, mixtures, database
             if element not in endpoint.keys():
                 endpoint[element] = 0
 
+    # Write and run endpoint calculations
     inputFileName = f'inputs/{property.replace(" ","_")}OfMixing-{phase}-{temperature}{tunit}.ti'
     with open(inputFileName, 'w') as inputFile:
         inputFile.write(f'data file              = {database}\n')
@@ -33,13 +34,11 @@ def propertyOfMixing(property, phase, temperature, endpoints, mixtures, database
         inputFile.write(f'number excluded except = 1\n')
         inputFile.write(f'phases excluded except = {phase}\n')
         inputFile.write(f'heat capacity          = {".TRUE." if property in ["enthalpy","entropy"] else ".FALSE."}\n')
-        inputFile.write(f'nCalc                  = {len(endpoints) + len(mixtures)}\n')
+        inputFile.write(f'nCalc                  = {len(endpoints)}\n')
         # Write endpoint calculations
         for endpoint in endpoints:
             inputFile.write(f'{temperature} {pressure} {" ".join([str(endpoint[element]) for element in elements])}\n')
-        # Write mixture calculations
-        for mixture in mixtures:
-            inputFile.write(f'{temperature} {pressure} {" ".join([str((1-mixture)*endpoints[0][element] + (mixture)*endpoints[1][element]) for element in elements])}\n')
+
     # Run calculation
     subprocess.run([thermochimica_path,inputFileName])
 
@@ -59,25 +58,48 @@ def propertyOfMixing(property, phase, temperature, endpoints, mixtures, database
         i += 1
         endpointProp.append(data[str(i)][property])
 
+    # Write and run mixture calculations
+    inputFileName = f'inputs/{property.replace(" ","_")}OfMixing-{phase}-{temperature}{tunit}.ti'
+    with open(inputFileName, 'w') as inputFile:
+        inputFile.write(f'data file              = {database}\n')
+        inputFile.write(f'temperature unit       = {tunit}\n')
+        inputFile.write(f'pressure unit          = {punit}\n')
+        inputFile.write(f'mass unit              = {munit}\n')
+        inputFile.write(f'nEl                    = {len(elements)} \n')
+        inputFile.write(f'iEl                    = {" ".join([str(thermoTools.atomic_number_map.index(element)+1) for element in elements])}\n')
+        inputFile.write(f'number excluded except = 1\n')
+        inputFile.write(f'phases excluded except = {phase}\n')
+        inputFile.write(f'heat capacity          = {".TRUE." if property in ["enthalpy","entropy"] else ".FALSE."}\n')
+        inputFile.write(f'nCalc                  = {len(mixtures)}\n')
+        # Write mixture calculations
+        for mixture in mixtures:
+            inputFile.write(f'{temperature} {pressure} {" ".join([str((1-mixture)*endpoints[0][element] + (mixture)*endpoints[1][element]) for element in elements])}\n')
+    # Run calculation
+    subprocess.run([thermochimica_path,inputFileName])
+
+    # Process output
+    f = open('thermoout.json',)
+    try:
+        data = json.load(f)
+        f.close()
+    except:
+        f.close()
+        print('Data load failed, aborting phase diagram update')
+        return
+
     print()
     mixtureProp = []
+    i = 0
     for mixture in mixtures:
         i += 1
         prop = data[str(i)][property] - ((1-mixture)*endpointProp[0] + (mixture)*endpointProp[1])
         mixtureProp.append(prop)
         print(f'{mixture} {prop}')
+        data[str(i)][f'{property} of mixing'] = prop
 
     # Save data
+    with open('thermoout.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
     subprocess.run(['cp','thermoout.json',f'{property.replace(" ","_")}OfMixing-{phase}-{temperature}{tunit}.json'])
 
-
-property = 'entropy'
-phase = 'MSFL'
-temperature = 1000
-tunit = 'C'
-# endpoints = [dict([('Li',1),('F',1)]),dict([('Pu',1),('F',3)])]
-endpoints = [dict([('K',1),('F',1)]),dict([('Ca',1),('F',2)])]
-mixtures = [i/10 for i in range(1,10)]
-database = 'mstdb/Models and Documentation/MSTDB-TC_V1.3_Fluorides_8-0.dat'
-
-propertyOfMixing(property, phase, temperature, endpoints, mixtures, database, tunit = tunit)
+    return mixtureProp
