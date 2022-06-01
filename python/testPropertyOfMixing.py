f56c62bef7409e61fc8fa713fd66b2dac3164190
maxposchmann <maxposchmann@gmail.com>
2022-06-01 15:21:19 -0400

    Add demo for property of mixing calculation

---
+++ python/testPropertyOfMixing.py
@@ -0,0 +1,15 @@
+import propertyOfMixing
+
+property = 'entropy'
+phase = 'BCCN'
+temperature = 1000
+tunit = 'C'
+endpoints = [dict([('Pd',1)]),dict([('Mo',1)])]
+ncalc = 10
+mixtures = [i/ncalc for i in range(1,ncalc)]
+database = 'data/Kaye_NobleMetals.dat'
+
+values = propertyOfMixing.propertyOfMixing(property, phase, temperature, endpoints, mixtures, database, tunit = tunit)
+
+for i in range(len(mixtures)):
+    print(f'{mixtures[i]} {values[i]}')
