190770a5878523bc82c2c7ebce3941174b958850
maxposchmann <maxposchmann@gmail.com>
2022-10-13 13:16:46 -0400

    Parse interpolation scheme overrides

--- src/parser/ParseCSDataBlockQKTO.f90
+++ src/parser/ParseCSDataBlockQKTO.f90
@@ -63,8 +63,13 @@ subroutine ParseCSDataBlockQKTO( i )
 
     implicit none
 
-    integer :: i, k
-    character(8),dimension(20)  :: cTempVec
+    integer :: i, j, k, l, iK, iT
+    character(8),dimension(3)  :: cTempVec
+    character(8)  :: cTemp
+    integer, dimension(6)  :: iTemp
+    integer, dimension(2)  :: iToop
+    integer                :: nToop
+
 
     ! Loop through excess parameters:
     LOOP_ExcessMixingQKTO: do
@@ -79,7 +84,64 @@ subroutine ParseCSDataBlockQKTO( i )
         ! Not implemented yet.
         if (iRegularParamCS(nParamCS+1,1) <= 0) then
             do k = 1, -iRegularParamCS(nParamCS+1,1)
-                read (1,*,IOSTAT = INFO) cTempVec(1:10)
+                nInterpolationOverrideCS(i) = nInterpolationOverrideCS(i) + 1
+                ! Have to read interpolation override line, unfortunately the formatting is a mess.
+                ! Specifically, strings and integers abut.
+                read (1,*,IOSTAT = INFO) iInterpolationOverrideCS(i,1), iInterpolationOverrideCS(i,2), cTempVec(1), & 
+                iTemp(1), cTempVec(2), iTemp(2), cTempVec(3), iTemp(3), iTemp(4), iInterpolationOverrideCS(i,4)
+
+                nToop = 0
+                ! Loop over strings and break them into string and integer parts
+                do j = 1, 3
+                    cTemp = cTempVec(j)
+                    ! First check if Kohler
+                    iK = INDEX(cTemp,'K')
+                    ! Then if Toop
+                    iT = INDEX(cTemp,'T')
+                    if (iK > 0) then
+                        if (j == 1) then
+                            READ(cTemp(1:iK-1),*) iInterpolationOverrideCS(i,3)
+                        else
+                            READ(cTemp(1:iK-1),*) iTemp(j+3)
+                        end if
+                    else if (iT > 0) then
+                        nToop = nToop + 1
+                        if (j == 1) then
+                            READ(cTemp(1:iT-1),*) iInterpolationOverrideCS(i,3)
+                        else
+                            READ(cTemp(1:iT-1),*) iTemp(j+3)
+                        end if
+                        ! Save constituents identified with Toop label
+                        READ(cTemp(iT+1:8),*) iToop(nToop)
+                    end if
+                end do
+
+                ! An asymmetric triad will have 2 Toop entries.
+                ! Frustratingly, these identify the NON-CONSTANT constituents.
+                ! So, back out the constant one here.
+                if (nToop == 2) then
+                    LOOP_checkAsym: do j = 1, 3
+                        do l = 1, 2
+                            if (iInterpolationOverrideCS(i,j) == iToop(l)) then
+                                ! Constituent NON-CONSTANT, keep going
+                                cycle LOOP_checkAsym
+                            end if
+                        end do
+                        ! If we get here, it is the constant constituent. Save for calculation.
+                        iInterpolationOverrideCS(i,5) = iInterpolationOverrideCS(i,j)
+                        exit LOOP_checkAsym
+                    end do LOOP_checkAsym
+                else if (nToop == 0) then
+                    ! Nothing, this is symmetric.
+                    ! Leave iInterpolationOverrideCS(i,5) = 0 to indicate symmetric.
+                else
+                    ! This shouldn't be possible
+                    nInterpolationOverrideCS(i) = nInterpolationOverrideCS(i) - 1
+                    iInterpolationOverrideCS(i,:) = 0
+                    print *, 'Interpolation override not understood, ignored.'
+                    print *,  iInterpolationOverrideCS(i,1), iInterpolationOverrideCS(i,2), cTempVec(1), & 
+                    iTemp(1), cTempVec(2), iTemp(2), cTempVec(3), iTemp(3), iTemp(4), iInterpolationOverrideCS(i,4)
+                end if
             end do
             exit LOOP_ExcessMixingQKTO
         end if
