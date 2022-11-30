dfbe8baace6a2f4c8c16a9859cb476cf837bac2e
maxposchmann <maxposchmann@gmail.com>
2022-11-30 10:44:51 -0500

    Add new settings to phase diagram input parsers

--- src/parser/ParseInputPhaseDiagram.f90
+++ src/parser/ParseInputPhaseDiagram.f90
@@ -310,6 +310,33 @@ subroutine ParseInputPhaseDiagram(cInputFileName,dTempLow,dTempHigh,dDeltaT,dXlo
           print *,  trim(cErrMsg)
           return
         endif
+      case ('fuzzy','fuzzy stoichiometry','fuzzystoichiometry','fuzzy_stoichiometry',&
+        'Fuzzy','Fuzzy Stoichiometry','FuzzyStoichiometry','Fuzzy_Stoichiometry')
+        read(cValue,*,IOSTAT = INFO) lFuzzyStoich
+        if (INFO /= 0) then
+          INFOThermo = 54
+          write (cErrMsg, '(A47,I10)') 'Cannot read fuzzy stoichiometry mode on line: ', iCounter
+          print *,  trim(cErrMsg)
+          return
+        end if
+      case ('fuzzy mag','fuzzy magnitude','fuzzymagnitude','fuzzy_magnitude',&
+        'Fuzzy Mag','Fuzzy Magnitude','FuzzyMagnitude','Fuzzy_Magnitude')
+        read(cValue,*,IOSTAT = INFO) dFuzzMag
+        if (INFO /= 0) then
+          INFOThermo = 54
+          write (cErrMsg, '(A52,I10)') 'Cannot read fuzzy stoichiometry magnitude on line: ', iCounter
+          print *,  trim(cErrMsg)
+          return
+        end if
+      case ('gibbs', 'gibbs min', 'gibbs minimum', 'gibbs_minimum', 'gibbsmin',&
+        'Gibbs', 'Gibbs Min', 'Gibbs Minimum', 'Gibbs_Minimum', 'GibbsMin')
+        read(cValue,*,IOSTAT = INFO) lGibbsMinCheck
+        if (INFO /= 0) then
+          INFOThermo = 54
+          write (cErrMsg, '(A46,I10)') 'Cannot read Gibbs energy check mode on line: ', iCounter
+          print *,  trim(cErrMsg)
+          return
+        end if
       case default
         write (cErrMsg, '(A34,I10)') 'Input tag not recognized on line: ', iCounter
         print *,  trim(cErrMsg)
