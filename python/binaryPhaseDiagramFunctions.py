7a4875cfc1138a0fb49b7d7689664a401597bce4
maxposchmann <maxposchmann@gmail.com>
2022-12-07 13:30:18 -0500

    Move pdPoint class to new common file

--- python/binaryPhaseDiagramFunctions.py
+++ python/binaryPhaseDiagramFunctions.py
@@ -16,6 +16,7 @@ from functools import reduce
 import operator
 import csv
 import thermoTools
+from phaseDiagramCommonFunctions import *
 
 phaseIncludeTol = 1e-8
 
@@ -848,18 +849,3 @@ class diagram:
                 newData.append(newrow)
         self.experimentalData.append(np.array(newData))
         self.experimentNames.append(expName)
-
-class pdPoint:
-    def __init__(self,elements,temperature,concentration,phases,phaseConcentrations,energy,iterations):
-        self.t = temperature
-        self.runConcentration = concentration
-        self.phaseConcentrations = phaseConcentrations
-        self.phases = phases
-        self.details = f'Temperature = {temperature:6.2f}\n'
-        for elem,conc in zip(elements,self.runConcentration):
-            self.details = self.details + f'Moles of {elem} = {conc:9.8f}\n'
-        for phase,conc in zip(self.phases,self.phaseConcentrations):
-            self.details = self.details + f'{phase} at {conc:5.4f}\n'
-        self.details = self.details + f'Integral Gibbs Energy = {energy:.2f}\n'
-        self.details = self.details + f'Number of GEM iterations = {iterations}'
-        self.suppressed = False
--- python/pseudoBinaryPhaseDiagramFunctions.py
+++ python/pseudoBinaryPhaseDiagramFunctions.py
@@ -6,6 +6,7 @@ import scipy.optimize
 from itertools import cycle
 import csv
 import thermoTools
+from phaseDiagramCommonFunctions import *
 
 # For boundaries of phase regions where both sides have (# phases) < (# elements), only plot points within phaseFractionTol of the boundary
 phaseFractionTol = 1e-2
@@ -377,18 +378,3 @@ class diagram:
                 newData.append(newrow)
         self.experimentalData.append(np.array(newData))
         self.experimentNames.append(expName)
-
-class pdPoint:
-    def __init__(self,elements,temperature,concentration,phases,phaseConcentrations,energy,iterations):
-        self.t = temperature
-        self.runConcentration = concentration
-        self.phaseConcentrations = phaseConcentrations
-        self.phases = phases
-        self.details = f'Temperature = {temperature:6.2f}\n'
-        for elem,conc in zip(elements,self.runConcentration):
-            self.details = self.details + f'Moles of {elem} = {conc:9.8f}\n'
-        for phase,conc in zip(self.phases,self.phaseConcentrations):
-            self.details = self.details + f'{phase} at {conc:5.4f}\n'
-        self.details = self.details + f'Integral Gibbs Energy = {energy:.2f}\n'
-        self.details = self.details + f'Number of GEM iterations = {iterations}'
-        self.suppressed = False
