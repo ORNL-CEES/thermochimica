16b8b7207257a245113c28d72301d29e148d8240
maxposchmann <maxposchmann@gmail.com>
2022-12-19 11:48:59 -0500

    Try new randomization scheme for miscibility gap phases

--- src/setup/RandomizeStoichiometry.f90
+++ src/setup/RandomizeStoichiometry.f90
@@ -1,5 +1,5 @@
 
-    !-------------------------------------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------------------------------------
     !
     !> \file        RandomizeStoichiometry.f90
     !> \brief       Fuzzy stoichiometry operations.
@@ -21,27 +21,61 @@
     !> \param       dFuzzMag        A double real scalar representing magnitude of perturbation to the
     !!                                  stoichiometries in thermodynamic database.
     !
-    !-------------------------------------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------------------------------------
 
 
 subroutine RandomizeStoichiometry
 
     USE ModuleThermoIO
     USE ModuleThermo
+    USE ModuleGEMSolver, ONLY: lMiscibility
 
     implicit none
 
-    integer :: i, j
+    integer :: i, j, k, n
+    integer, allocatable :: lastSeed(:)
+    real(8) :: rValue
 
     ! Fuzzy stoichiometry
     allocate(dStoichSpeciesUnFuzzed(nSpecies,nElements))
     
-    do i = 1, nSpecies
+    call random_seed(size = n)
+    if (allocated(lastSeed)) deallocate(lastSeed)
+    allocate(lastSeed(n))
+    lastSeed = 0
+
+    ! Species in solution phases
+    do k = 1, nSolnPhasesSys
+        ! Reset the random seed so all phases start the same
+        ! This is for miscibility gap phases: could change to only apply to those
+        ! call SRAND(7)
+        if (lMiscibility(k)) then
+            call RANDOM_SEED(put=lastSeed)
+        else
+            call RANDOM_SEED(get=lastSeed)
+        end if
+        
+        do i = nSpeciesPhase(k-1) + 1, nSpeciesPhase(k)
+            do j = 1, nElements
+                dStoichSpeciesUnFuzzed(i,j) = dStoichSpecies(i,j)
+                if (dMolesElement(j) < 1D-5) cycle
+                if (dStoichSpecies(i,j) > 0D0) then
+                    call RANDOM_NUMBER(rValue)
+                    dStoichSpecies(i,j) = dStoichSpecies(i,j) + 2D0 * (rValue - 0.5D0) * dFuzzMag
+                end if
+                print *, cSpeciesName(i), dStoichSpecies(i,:)
+            end do
+        end do
+    end do
+
+    ! Stoichiometric phases
+    do i = nSpeciesPhase(nSolnPhasesSys) + 1, nSpecies
         do j = 1, nElements
             dStoichSpeciesUnFuzzed(i,j) = dStoichSpecies(i,j)
             if (dMolesElement(j) < 1D-5) cycle
             if (dStoichSpecies(i,j) > 0D0) then
-                dStoichSpecies(i,j) = dStoichSpecies(i,j) + 2D0 * (RAND(0) - 0.5D0) * dFuzzMag
+                call RANDOM_NUMBER(rValue)
+                dStoichSpecies(i,j) = dStoichSpecies(i,j) + 2D0 * (rValue - 0.5D0) * dFuzzMag
             end if
         end do
     end do
